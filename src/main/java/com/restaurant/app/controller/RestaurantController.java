4cd40ec0be0c279c5263fad57e437095c2533329
kumar dhakal <dhakalkumar10@gmail.com>
2020-02-09 16:35:04 +0545

    first commit

---
+++ src/main/java/com/restaurant/app/controller/RestaurantController.java
@@ -0,0 +1,14 @@
+package com.restaurant.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RestaurantController {
+
+	@GetMapping("/")
+	public String getIndex() {
+		
+		return "index";
+	}
+}
